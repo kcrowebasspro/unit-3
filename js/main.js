5fc133d498f5f2c330105da29f1ace3be0744570
Kevin Crowe <339920+kcrowebasspro@users.noreply.github.com>
2025-03-29 16:38:04 -0400

    Join CSV to geojson

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function setMap(){
     //create Albers equal area conic projection centered on Iowa
     var projection = d3.geoAlbers()
         .center([8, 42.3])
-        .rotate([101, 0, 0])
+        .rotate([102, 0, 0])
         .parallels([29.5, 45.5])
         .scale(6500)
         .translate([width / 2, height / 2]);
@@ -42,6 +42,34 @@ function setMap(){
         //convert topojson to geojson
         var iowaZips = topojson.feature(zips, zips.objects.iowa_ZIPs).features;
        
+        //variables for data join
+        var attrArray = ["avgPrem2018", "avgPrem2022", "premChange", "premPctChange", "nonRenewRate", 
+            "nonPayRate", "claimSeverity", "claimFrequency", "lossRatio"];
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+            var csvZip = csvData[i]; //the current region
+            var csvKey = csvZip.GEOID20; //the CSV primary key
+
+            //loop through geojson regions to find correct region
+            for (var a=0; a<iowaZips.length; a++){
+
+                var geojsonProps = iowaZips[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.GEOID20; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr){
+                        var val = parseFloat(csvZip[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                };
+            };
+        };
+
+        
         // add the Iowa zip codes to the map
         var zipCodes = map.selectAll(".zips")
             .data(iowaZips).enter()
